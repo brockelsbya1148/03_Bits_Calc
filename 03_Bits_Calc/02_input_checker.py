48eac85ab72170a083d84df4ca4570d4db5849cb
brockelsbya1148 <110875387+brockelsbya1148@users.noreply.github.com>
2022-09-12 09:33:54 +1200

    Add files via upload

---
+++ 03_Bits_Calc/02_input_checker.py
@@ -0,0 +1,18 @@
+# Checks user choice is 'integer', 'text', or 'image'
+def user_choice():
+
+    valid = False
+    while not valid:
+
+        response = input("File type (integer / text / image): ").lower()
+
+        if response == "text" or response == "t":
+            return response
+
+        else:
+            print("Please choose a valid file type!")
+            print()
+
+
+# Main Routine goes here
+data_type = user_choice()
