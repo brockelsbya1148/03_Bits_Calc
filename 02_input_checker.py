e0bafd8d6a0fc8ceeed72c5a3ce244972193f32d
brockelsbya1148 <110875387+brockelsbya1148@users.noreply.github.com>
2022-09-14 13:03:15 +1200

    Add files via upload

--- 02_input_checker.py
+++ 02_input_checker.py
@@ -1,22 +1,4 @@
-# Functions go here
-
-# Puts series of symbols at start and end of text (for emphasis)
-def statement_generator(text, decoration):
-
-    # Make string with five characters
-    ends = decoration * 5
-
-    # Add decoration to start and end of statement
-    statement = "{}  {}  {}".format(ends, text, ends)
-
-    print()
-    print(statement)
-    print()
-
-    return ""
-
-
-# Checks user choice is 'integer', 'text' or 'image'
+# Checks user choice is 'integer', 'text', or 'image'
 def user_choice():
 
     valid = False
@@ -49,21 +31,9 @@ def user_choice():
 
 
 # Main Routine goes here
-
-# Heading
-statement_generator("Bit Calculator for Integers, Text & Images", "-")
-
-# Display instructions if user has not used the program before
-
-# Loop to allow multiple calculations per session
 keep_going = ""
 while keep_going == "":
-
-    # Ask the user for the file type
     data_type = user_choice()
     print("You chose", data_type)
-    # For integers, ask for integer
-    # (must be an integer more than / equal to 0)
 
-    # For images, ask for width and height
-    
+    print()
