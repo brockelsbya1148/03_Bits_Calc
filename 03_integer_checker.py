fbc288640a0b980bcf0b692fee2725c988bd03a6
brockelsbya1148 <110875387+brockelsbya1148@users.noreply.github.com>
2022-09-16 10:19:55 +1200

    Add files via upload

--- 03_integer_checker.py
+++ 03_integer_checker.py
@@ -1,17 +1,18 @@
 # checks input is more than zero
-def num_check(question):
+def num_check(question, low):
     valid = False
     while not valid:
 
-        error = "Please enter a number that is more than zero"
+        error = "Please enter an integer that is more than "
+        "(or equal to) {}".format(low)
 
         try:
 
             # ask user to enter a number
-            response = float(input(question))
+            response = int(input(question))
 
             # checks number is more than zero
-            if response > 0:
+            if response >= low:
                 return response
 
             # outputs error if input is invalid
